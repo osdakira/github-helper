eb15780b089841871df8700f752cc94dec00b155
Akira Osada <osd.akira@gmail.com>
2019-10-25 21:01:06 +0900

    feat: can toggle Show/Hide TimelineItems using popup.html

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,4 +1,24 @@
 import "../css/popup.css";
-import hello from "./popup/example";
 
-hello();
+function registerEvent() {
+  document.addEventListener("change", (e) => {
+    const key = e.target.value;
+    const val = e.target.checked;
+    chrome.storage.local.set({ [key]: val }, () => {});
+  });
+}
+
+function restoreCheckedStatuses() {
+  document.querySelectorAll("input").forEach((input) => {
+    chrome.storage.local.get(input.value, (result) => {
+      input.checked = result[input.value];
+    });
+  });
+}
+
+function init() {
+  restoreCheckedStatuses();
+  registerEvent();
+}
+
+init();
